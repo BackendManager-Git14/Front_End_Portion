c94750e16122c7dac7b2111ac6907422f597f15e
BackendManager-Git14 <aksharpp2910@gmail.com>
2024-09-24 01:40:27 +0530

    connect file done in designing

---
+++ Connect Content/connect.js
@@ -0,0 +1,22 @@
+const input = document.querySelectorAll('.input-field');
+
+input.forEach((inp) => {
+    inp.addEventListener('focus',function(){
+        inp.classList.add('active');
+    })
+    inp.addEventListener('blur',function(){
+        if(inp.value != "") return;
+        inp.classList.remove('active');
+    })
+})
+
+/* logic of sliding form switch */
+
+const toggleform = document.querySelectorAll('.toggle');
+const mainsection = document.querySelector('main');
+
+toggleform.forEach((btn) => {
+    btn.addEventListener('click',function(){
+        mainsection.classList.toggle('form-mode');
+    })
+})
